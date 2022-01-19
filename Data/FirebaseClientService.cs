95ba9717afe785b670fe90c8d060bfd675363443
Jakub Krasucki <jakub.krasucki@tzmo-global.com>
2022-01-19 17:42:06 +0100

    Change MAUI to Xamarin

--- Data/FirebaseClientService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Firebase.Database;
-
-namespace Hello_Earth_2.Data
-{
-    public class FirebaseClientService
-    {
-        public static FirebaseClient firebaseClient = new FirebaseClient("https://helloearth2-5887b-default-rtdb.europe-west1.firebasedatabase.app/");
-    }
-}
