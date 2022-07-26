bb0d19e6c33437e8e2aeb74421f8da563d0a2301
Olena Savelieva <savelieva372@gmail.com>
2022-07-26 20:52:23 +0200

    add splash_screen

---
+++ Hello_Earth_2/Hello_Earth_2.Android/SplashActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_Earth_2.Droid
+{
+    //[Activity(Theme = "@style/Theme.Splash", MainLauncher = false, NoHistory = true)]
+    //public class SplashActivity : Activity
+    //{
+    //    protected override void OnCreate(Bundle savedInstanceState)
+    //    {
+    //        base.OnCreate(savedInstanceState);
+    //        System.Threading.Thread.Sleep(3000);
+    //        StartActivity(typeof(MainActivity));
+    //    }
+    //}
+}
